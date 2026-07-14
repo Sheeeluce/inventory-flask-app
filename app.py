39af4420b7ecbe36fe3762862fb9239097855448
Lucy <lucymwago9@gmail.com>
2026-07-14 09:05:08 +0300

    get url and 404

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def home():
     return "Inventory Management API"
 
-@app.route("/items")
-def get_items():
-    return jsonify(inventory)
+@app.route("/items/<int:item_id>")
+def get_items(item_id):
+
+    for item in inventory:
+        if item["id"] == item_id:
+            return jsonify(item)
+    return jsonify({"error": "Item not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
