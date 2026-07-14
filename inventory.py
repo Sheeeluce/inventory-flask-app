7edef50b8f04a60febb0ea20531f00b52b48d850
Lucy <lucymwago9@gmail.com>
2026-07-14 08:44:07 +0300

    inventory

---
+++ inventory.py
@@ -0,0 +1,20 @@
+inventory = [
+    {
+        "id": 1,
+        "barcode": "4782983693846",
+        "product-name": "Nutella",
+        "brand": "Ferrero",
+        "ingredients": "Sugar, palm oil, cocoa...",
+        "quantity": 15,
+        "price": 1250
+    },
+    {
+        "id": 2,
+        "barcode": "987652567234",
+        "product-name": "Coke-Zero",
+        "brand": "CocaCola",
+        "ingredients": "Carbonated water...",
+        "quantity": 20,
+        "price": 120  
+    }
+]
