d8f5d09d69b2ddd7adb83b97265ff5270d8631d8
Lucy <lucymwago9@gmail.com>
2026-07-14 08:55:48 +0300

    endpoint functional

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from inventory import inventory
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Inventory Management API"
+
+@app.route("/items")
+def get_items():
+    return jsonify(inventory)
 
 if __name__ == "__main__":
     app.run(debug=True)
