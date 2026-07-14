3d647e52583c0cee464898279f15e523e231e22f
Lucy <lucymwago9@gmail.com>
2026-07-14 09:18:39 +0300

    post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from inventory import inventory
 
 
@@ -15,6 +15,23 @@ def get_items(item_id):
         if item["id"] == item_id:
             return jsonify(item)
     return jsonify({"error": "Item not found"}), 404
+
+@app.route("/items", methods = ["POST"])
+def add_item():
+    data = request.get_json()
+    new_item = {
+        "id": len(inventory) + 1,
+        "barcode": data["barcode"],
+        "product_name": data["product_name"],
+        "brand": data["brand"],
+        "ingredients": data["ingredients"],
+        "quantity": data["quantity"],
+        "price": data["price"]
+    }
+
+    inventory.append(new_item)
+
+    return jsonify(new_item), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
